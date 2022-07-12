ea921241194211f1d4a700b4e5faa72247f72a3b
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-07-12 19:53:37 +0200

    Update login and register styles

--- lib/Auth/user.dart
+++ lib/Auth/user.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UserPage extends StatelessWidget {
@@ -7,19 +8,26 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(user.email!),
-            ElevatedButton.icon(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              label: const Text("Logout"),
-              icon: const Icon(Icons.lock),
-            )
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            CupertinoButton.filled(
+              onPressed: () => FirebaseAuth.instance.signOut(),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Icon(Icons.lock),
+                  Text("Logout"),
+                ],
+              ),
+            ),
           ],
         ),
       ),
